ee526ccfc63226e8752cf8297427924ac0630c73
maganadev <maganadev>
2022-12-13 21:02:04 -0800

    Customer Order Note

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/Orderline.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/Orderline.js
@@ -1,7 +1,10 @@
+
+
 odoo.define('point_of_sale.Orderline', function(require) {
     'use strict';
 
     const PosComponent = require('point_of_sale.PosComponent');
+    const ProductScreen = require('point_of_sale.ProductScreen');
     const Registries = require('point_of_sale.Registries');
 
     class Orderline extends PosComponent {
@@ -10,6 +13,16 @@ odoo.define('point_of_sale.Orderline', function(require) {
         }
         lotIconClicked() {
             this.trigger('edit-pack-lot-lines', { orderline: this.props.line });
+        }
+        // ***** Added function ***** //
+        setNote() {
+            const selectedOrderline = this.env.pos.get_order().get_selected_orderline();
+
+            var note = document.getElementById("order_note_input").value;
+
+            selectedOrderline.set_customer_note(note);
+            
+            return;
         }
         get addedClasses() {
             return {
@@ -20,9 +33,18 @@ odoo.define('point_of_sale.Orderline', function(require) {
             return this.props.line.get_customer_note();
         }
     }
+
+
     Orderline.template = 'Orderline';
 
     Registries.Component.add(Orderline);
 
     return Orderline;
 });
+
+
+
+
+
+
+
